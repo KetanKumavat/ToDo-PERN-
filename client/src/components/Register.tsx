82d9ed4c87b9c59f36d67afc10f1ce3c3cab7977
Ketan <127680348+KetanKumavat@users.noreply.github.com>
2024-07-07 01:06:39 +0530

    Update Register.tsx

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -11,7 +11,7 @@ function Login() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/v1/user/register",
+        "https://todo-pern-n60f.onrender.com/api/v1/user/register",
         {
           username,
           password,
